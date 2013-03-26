30e4873bc71a2c034cbf00ac964206a37f9ac205
Ted Killilea <lrrs259@gmail.com>
2013-03-25 20:25:28 -0400

    stable - usable version

--- assetsAMD/js/views/book.js
+++ assetsAMD/js/views/book.js
@@ -1,7 +1,7 @@
 // book.js: Ted Killilea June 2012  @t2k_nyc
 //  the book item view: when clicked display the modal detailview
 //  let the app handle this
-define(['underscoreM', 'marionette',  'templates', 'views/bookdetail', 'vent'], function(_, Marionette, templates, BookDetailView, vent) {
+define(['underscoreM', 'marionette','templates', 'views/bookdetail', 'vent'], function(_, Marionette, templates, BookDetailView, vent) {
     'use strict';
 
     return Marionette.ItemView.extend({
